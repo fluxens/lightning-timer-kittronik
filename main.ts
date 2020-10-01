04f739a63bdd12b6a0c2f0398c133853eeafae3c
fluxens <haftbar@gmail.com>
2020-10-01 10:26:52 +0200

    added minutes

--- main.ts
+++ main.ts
@@ -14,6 +14,9 @@ input.onButtonPressed(Button.A, function () {
     for (let index = 0; index <= 60; index++) {
         haloDisplay.setZipLedColor(index, kitronik_halo_hd.colors(ZipLedColors.Green))
         haloDisplay.show()
+        if (index % 15 == 0) {
+            basic.showNumber(5 - index / 15)
+        }
         basic.pause(4000)
     }
     for (let index = 0; index <= 60; index++) {
@@ -26,6 +29,7 @@ input.onButtonPressed(Button.A, function () {
         haloDisplay.show()
         basic.pause(500)
     }
+    basic.showNumber(0)
     for (let index = 0; index <= 6; index++) {
         haloDisplay.clear()
         haloDisplay.show()
@@ -34,11 +38,10 @@ input.onButtonPressed(Button.A, function () {
         haloDisplay.show()
         basic.pause(200)
     }
-    basic.showIcon(IconNames.Happy)
-    basic.pause(2000)
     basic.clearScreen()
     haloDisplay.clear()
     haloDisplay.show()
+    basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.AB, function () {
     haloDisplay.clear()
@@ -48,43 +51,47 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     haloDisplay.clear()
     basic.showNumber(5)
-    basic.pause(200)
+    basic.pause(100)
     basic.showNumber(4)
-    basic.pause(200)
+    basic.pause(100)
     basic.showNumber(3)
-    basic.pause(200)
+    basic.pause(100)
     basic.showNumber(2)
-    basic.pause(200)
+    basic.pause(100)
     basic.showNumber(1)
+    basic.pause(100)
     basic.clearScreen()
     for (let index = 0; index <= 60; index++) {
         haloDisplay.setZipLedColor(index, kitronik_halo_hd.colors(ZipLedColors.Green))
         haloDisplay.show()
+        if (index % 15 == 0) {
+            basic.showNumber(5 - index / 15)
+        }
         basic.pause(200)
     }
     for (let index = 0; index <= 60; index++) {
         haloDisplay.setZipLedColor(index, kitronik_halo_hd.colors(ZipLedColors.Orange))
         haloDisplay.show()
-        basic.pause(200)
+        basic.pause(150)
     }
     for (let index = 0; index <= 60; index++) {
         haloDisplay.setZipLedColor(index, kitronik_halo_hd.colors(ZipLedColors.Red))
         haloDisplay.show()
-        basic.pause(200)
+        basic.pause(150)
     }
+    basic.showNumber(0)
     for (let index = 0; index <= 6; index++) {
         haloDisplay.clear()
         haloDisplay.show()
-        basic.pause(200)
+        basic.pause(150)
         haloDisplay.setColor(kitronik_halo_hd.colors(ZipLedColors.Red))
         haloDisplay.show()
-        basic.pause(200)
+        basic.pause(150)
     }
-    basic.showIcon(IconNames.Happy)
-    basic.pause(2000)
     basic.clearScreen()
     haloDisplay.clear()
     haloDisplay.show()
+    basic.showIcon(IconNames.Happy)
 })
 let haloDisplay: kitronik_halo_hd.ZIPHaloHd = null
 kitronik_halo_hd.setBuzzerPin()
